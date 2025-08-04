f912e520b99eae253167e4df77cd1c0436230347
WaranyooChuathon <waranyoo.chuathon@gmail.com>
2025-08-04 17:12:08 +0700

    Setup New subject

---
+++ controlled-and-uncontrolled-components/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { UncontrolledForm } from './UncontrolledForm'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <UncontrolledForm/>
+    </>
+  )
+}
+
+export default App
